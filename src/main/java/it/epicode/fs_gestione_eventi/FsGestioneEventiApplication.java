487512b446d8ad393cd3e9232221d4127834cfcb
GennaroSavastano <tantormainondormopiu@gmail.com>
2025-05-04 18:39:55 +0200

    Initial commit

---
+++ src/main/java/it/epicode/fs_gestione_eventi/FsGestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.fs_gestione_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsGestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsGestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/fs_gestione_eventi/FsGestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.fs_gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsGestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
